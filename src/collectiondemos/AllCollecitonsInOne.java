ffee9a736b6f017b26dbf0d5ab62fa2ad16169a1
RazEbrat <raz.tekschool@gmail.com>
2023-02-25 20:24:17 -0500

    These are the new changes in the file

---
+++ src/collectiondemos/AllCollecitonsInOne.java
@@ -0,0 +1,16 @@
+package collectiondemos;
+
+public class AllCollecitonsInOne {
+
+	public static void main(String[] args) {
+		
+		// this is to test push in git
+		
+		/*
+		 * 
+		 * this is just a random class just to bring changes in Collections
+		 */
+
+	}
+
+}
